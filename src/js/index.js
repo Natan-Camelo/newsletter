4d2ee426e15dc5a2cfaeaea556f6c18261dcb43d
Natan Camelo de Sousa <127790024+Natan-Camelo@users.noreply.github.com>
2024-02-23 19:32:29 -0300

    Delete src

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-// Objetivo: Criar uma mudança de cor do botão quando for interagir e um alerta para notificar no de envio.
-
-let click = window.document.querySelector(".botao-de-envio");
-click.addEventListener('click', clicar);
-
-function clicar() {
-    window.alert('Um e-mail de confirmação de cadrasto foi enviado para você.');
-}
